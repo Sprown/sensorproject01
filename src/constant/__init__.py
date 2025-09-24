d2f77e8983400929684dea1c94cee0130a956533
Sprown <hsoni.12515@gmail.com>
2025-09-24 19:46:49 +0530

    new testing

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,16 +1,26 @@
 import os
 
 
-AWS_S3_BUCKET_NAME = "Sensor_newServer"
+AWS_S3_BUCKET_NAME = "himanshu-buckets"
 MONGO_DATABASE_NAME = "pwskills"
 MONGO_COLLECTION_NAME = "waferfault"
 
 
 TARGET_COLUMN = "quality"
-MONGO_DB_URL="mongodb+srv://himanshu:Cuk6GH1DSoNgYtON@cluster0.fv0lm61.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DB_URL="mongodb+srv://himanshu:Himanshu-12515@cluster0.fv0lm61.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 MODEL_FILE_NAME = "model"
 MODEL_FILE_EXTENSION = ".pkl"
 
 artifact_folder =  "artifacts"
+
+
+# AWS_ACCESS_KEY_ID: AKIAU55B56FK3456YZHN
+# AWS_BUCKET_NAME: himanshu-buckets
+# AWS_DEFAULT_REGION: us-west-2 
+# AWS_ECR_REPO_URI: 339105149269.dkr.ecr.us-west-2.amazonaws.com/himanshu_ecr
+# AWS_SECRET_ACCESS_KEY: 8QWk769O2MzkV4BY52XqsmoypqpC0EdoiZUe4eOz
+# MONGO_DB_URL: mongodb+srv://himanshu:Himanshu-12515@cluster0/pwskills
+
+# mongodb+srv://himanshu:Himanshu-12515@cluster0.fv0lm61.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
